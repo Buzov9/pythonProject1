2977f2b45ec8fafcd59b7d4f76b1802fe78fff97
buzov9 <koskinfedor9@gmail.com>
2024-09-29 19:31:15 +0800

    1

--- module_5_4.py
+++ module_5_4.py
@@ -1,15 +1,87 @@
-print(int.__mro__)
-class User:
-    def __init__(self, *args, **kwargs):
-        print('hi')
-        self.args = args
-        for key, varues in kwargs.items():
-            setattr(self, key, varues)
+import copy
 
 
-other = [1, 2, 3]
-user = {'name':'Den', 'age': 25, 'gender':'male'}
+class House:
 
-user1 = User(*other, **user)
-print(user1.args)
-print(user1.name)
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        House.houses_history.append(args)
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+
+        print (f'{self.name} снесён,но он останется в истории')
+
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(new_floor):
+                print(i + 1)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors == other
+        elif isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, House):
+            return House(self.name, self.number_of_floors + value.number_of_floors)
+        elif isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+        else:
+            return NotImplemented
+
+    def __radd__(self, value):
+        if isinstance(value, House):
+            return House(self.name, value.number_of_floors + self.number_of_floors)
+        elif isinstance(value, int):
+            return House(self.name, value + self.number_of_floors)
+        else:
+            return NotImplemented
+
+        def __iadd__(self, value):
+
+            self.number_of_floors += value
+            return self
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 15)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
