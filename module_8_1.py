dae8d68620e456388ffa443b9c85eef65bad3dd8
buzov9 <koskinfedor9@gmail.com>
2024-10-15 13:47:01 +0900

    1

---
+++ module_8_1.py
@@ -0,0 +1,15 @@
+def add_everything_up(a, b):
+    result = None
+    try:
+        result = a + b
+        return result
+    except TypeError:
+        a = str(a)
+        b = str(b)
+        result = a + b
+        return result
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
