a53025dea14d861c8c8f2418c909de14319d13d3
buzov9 <koskinfedor9@gmail.com>
2024-10-31 21:40:13 +0900

    1

--- Pice.py
+++ Pice.py
@@ -1,6 +1,19 @@
-from pprint import pprint
+import os
+# print(os.getcwd())#текущая директория
+#
+# if os.path.exists('new_file'): #True, если такая директория существуе
+#     os.chdir('new_file')#изменяет
+# else:
+#     os.mkdir('new_file')#создает
+#     os.chdir('new_file')
+# print(os.getcwd())
+# # os.makedirs(r'new_file\in_file')#создание директории с определенным уровнем вложенности
+# print(os.listdir())
+# for i in os.walk('.'):
+#     print(i)
+# print(chr('A'))
 
-name = 'repka2.txt'
-file = open(name, 'a')
-file.read()
-file.close()
+a = 's'
+print(a)
+a += 0
+print(a)
--- module_8_2.py
+++ module_8_2.py
@@ -1,6 +1,10 @@
 def personal_sum(numbers):
     result = 0
     incorrect_data = 0
+    try:
+        len(numbers)
+    except:
+        return 0, 0
 
     for i in numbers:
         try:
@@ -9,26 +13,19 @@ def personal_sum(numbers):
             incorrect_data += 1
             print(f'Некорректный тип данных для подсчёта суммы - {i}')
     return result, incorrect_data
-
-
 def calculate_average(numbers):
-    count = 0
-    if isinstance(numbers, (tuple, list, str)):
-        for i in numbers:
-            sphere = 0
-            try:
-                sphere += i
-            except TypeError:
-               pass
-            else:
-                count += 1
-
-        try:
-            return personal_sum(numbers)[0] / count
-        except ZeroDivisionError:
-            return 0
-    else:
+    sum_num, incorrect_data = personal_sum(numbers)
+    try:
+        total_numbers = len(numbers) - incorrect_data
+        return sum_num / total_numbers
+    except ZeroDivisionError:
         print(f'В numbers записан некорректный тип данных')
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+
+
+
 print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
