4129e53f2c81c4571346fc928de06aeaa3fdd864
buzov9 <koskinfedor9@gmail.com>
2024-11-13 19:10:42 +0900

    1

---
+++ Module_10_2.py
@@ -0,0 +1,29 @@
+import threading
+import time
+from tqdm import tqdm
+
+
+class Knight(threading.Thread):
+    def __init__(self, name, power):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        enemy = 100
+        day = 0
+        while enemy:
+            """for i in tqdm(range(10), desc="проходит день"):
+                time.sleep(0.1)"""
+            time.sleep(1)
+            day += 1
+            enemy -= self.power
+            print(f'{self.name} сражается {day} дней, осталось {enemy} воинов')
+        print(f'{self.name} одержал победу спустя {day} дней')
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+first_knight.start()
+second_knight.start()
--- Pice.py
+++ Pice.py
@@ -12,6 +12,30 @@ import os
 # for i in os.walk('.'):
 #     print(i)
 # print(chr('A'))
-sum_ = 11 % 7
-print(sum_)
+
+
+# class MyThread(threading.Thread):
+#     def __init__(self, name, counter, delay):
+#         threading.Thread.__init__(self)
+#         self.name = name
+#         self.counter = counter
+#         self.delay = delay
+#
+#     def timer(self, name, counter, delay):
+#         while counter:
+#             time.sleep(delay)
+#             print(f'{name} {time.ctime(time.time())}')
+#             counter -= 1
+#
+#
+#     def run(self):
+#         print(f"поток {self.name} запущен")
+#         self.timer(self.name, self.counter, self.delay)
+#         print(f"поток {self.name} завершен")
+
+from tqdm import tqdm
+import time
+
+for i in tqdm(range(10), desc="проходит день"):
+    time.sleep(0.1)
 
