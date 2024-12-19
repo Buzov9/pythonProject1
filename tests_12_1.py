7b7ff0f32547115fae6126acfe664117756c677e
buzov9 <koskinfedor9@gmail.com>
2024-12-19 16:15:42 +0800

    1

---
+++ tests_12_1.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        test_walk = Runner("test_obj_walk")
+        for i in range(10):
+            test_walk.walk()
+        self.assertEqual(test_walk.distance, 500)
+
+    def test_run(self):
+        test_run = Runner("test_obj_run")
+        for i in range(10):
+            test_run.run()
+        self.assertEqual(test_run.distance, 100)
+
+    def test_challenge(self):
+        test_obj1 = Runner('test_obj1')
+        test_obj2 = Runner('test_obj2')
+        for i in range(10):
+            test_obj1.walk()
+            test_obj2.run()
+        self.assertNotEqual(test_obj1.distance, test_obj2.distance)
